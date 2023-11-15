2d71c4592ebdb3ccf86421316941a9a27acd3cac
192211241 <122424318+Abinaya2311@users.noreply.github.com>
2023-11-15 10:52:55 +0530

    Update dining philosophers.c

--- DAY 2 PROGRAMS/dining philosophers.c
+++ DAY 2 PROGRAMS/dining philosophers.c
@@ -1,59 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-
-#define NUM_PHILOSOPHERS 5
-
-pthread_t philosophers[NUM_PHILOSOPHERS];
-pthread_mutex_t forks[NUM_PHILOSOPHERS];
-
-void *philosopher(void *arg) {
-    int id = *(int *)arg;
-    int left_fork = id;
-    int right_fork = (id + 1) % NUM_PHILOSOPHERS;
-	int i;
-    for(i=0;i<=1;i++) {
-        // Thinking
-        printf("Philosopher %d is thinking.\n", id);
-        usleep(rand() % 1000000);
-
-        // Pick up forks
-        pthread_mutex_lock(&forks[left_fork]);
-        pthread_mutex_lock(&forks[right_fork]);
-
-        // Eating
-        printf("Philosopher %d is eating.\n", id);
-        usleep(rand() % 1000000);
-
-        // Put down forks
-        pthread_mutex_unlock(&forks[left_fork]);
-        pthread_mutex_unlock(&forks[right_fork]);
-    }
-
-    return NULL;
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+sem_t room;
+sem_t chopstick[5];
+void * philosopher(void *);
+void eat(int);
+int main()
+{
+	int i,a[5];
+	pthread_t tid[5];
+	sem_init(&room,0,4);
+	for(i=0;i<5;i++)
+		sem_init(&chopstick[i],0,1);	
+	for(i=0;i<5;i++){
+		a[i]=i;
+		pthread_create(&tid[i],NULL,philosopher,(void *)&a[i]);
+	}
+	for(i=0;i<5;i++)
+		pthread_join(tid[i],NULL);
 }
-
-int main() {
-    srand(time(NULL));
-	int i;
-    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_mutex_init(&forks[i], NULL);
-    }
-
-    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
-        int *id = malloc(sizeof(int));
-        *id = i;
-        pthread_create(&philosophers[i], NULL, philosopher, id);
-    }
-
-    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_join(philosophers[i], NULL);
-    }
-
-    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_mutex_destroy(&forks[i]);
-    }
-
-    return 0;
+void * philosopher(void * num)
+{
+	int phil=*(int *)num;
+	sem_wait(&room);
+	printf("\nPhilosopher %d has entered room",phil);
+	sem_wait(&chopstick[phil]);
+	sem_wait(&chopstick[(phil+1)%5]);
+	eat(phil);
+	sleep(2);
+	printf("\nPhilosopher %d has finished eating",phil);
+	sem_post(&chopstick[(phil+1)%5]);
+	sem_post(&chopstick[phil]);
+	sem_post(&room);
+}
+void eat(int phil)
+{
+	printf("\nPhilosopher %d is eating",phil);
 }
